8fc8c662ded5304b2cced12900f87871a330027d
root <root@ip-172-31-1-253.us-east-2.compute.internal>
2023-01-30 09:30:22 +0000

    commit multiple variable 30/1/2023

---
+++ shell_scripting/multiple_variable.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+#
+# define  multiple variable 
+#
+name=asmita
+age=27
+profession=cloud_enggineer
+team=special_mock_group
+
+#calling variable
+ echo "i am a part of $team team, my age is $age old , my name is $name and i am $profession"
